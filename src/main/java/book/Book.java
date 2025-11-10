e4949d7aab49834259129a66c2a54591dcc5e809
perepilka <yaroslavperepilka@gmail.com>
2025-11-10 21:05:08 +0100

    refactor: remove unused getter and setter methods in Book class

--- src/main/java/book/Book.java
+++ src/main/java/book/Book.java
@@ -12,30 +12,6 @@ public class Book {
         this.author = author;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
     @Override
     public String toString() {
         return "Book{" +
