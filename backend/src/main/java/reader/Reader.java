553f225e8dd0b5ec9c69c25cffc3fa83e0a8e2ff
perepilka <yaroslavperepilka@gmail.com>
2025-11-10 20:06:38 +0100

    feat: add Reader class

---
+++ backend/src/main/java/reader/Reader.java
@@ -0,0 +1,20 @@
+package reader;
+
+public class Reader {
+
+    private Long id;
+    private String name;
+
+    public Reader(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Reader{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
