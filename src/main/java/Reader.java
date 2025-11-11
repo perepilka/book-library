4c2bc3984d089aec06f27d41491bf04a680e668c
perepilka <yaroslavperepilka@gmail.com>
2025-11-11 18:14:58 +0100

    refactor: change Reader class to record

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,18 +1,2 @@
-public class Reader {
-
-    private Long id;
-    private String name;
-
-    public Reader(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Reader{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
+public record Reader(Long id, String name) {
 }
