b31a2cb4f28e0ec402f7fcd4d5021c7053f33ecb
perepilka <yaroslavperepilka@gmail.com>
2025-11-11 18:14:52 +0100

    refactor: change Book class to record

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,21 +1,2 @@
-public class Book {
-
-    private Long id;
-    private String name;
-    private String author;
-
-    public Book(Long id, String name, String author) {
-        this.id = id;
-        this.name = name;
-        this.author = author;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", author='" + author + '\'' +
-                '}';
-    }
+public record Book(Long id, String name, String author) {
 }
